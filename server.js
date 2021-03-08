df2e057d4b98199b99909af5b0e2fb640acfaab5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-08 00:06:50 -0500

    working on front-end

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
@@ -93,7 +94,19 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals)
     res.json(animal);
   }
-})
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
